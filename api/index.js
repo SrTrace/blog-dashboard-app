0bcca46054a006c1e291272ab6404a6747a6b8c0
SrTrace <levasho04sergey@gmail.com>
2024-11-10 08:53:06 +0200

    set connection to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import 'dotenv/config'
+
+mongoose.connect(process.env.MONGO)
+  .then(() => console.log('MongoDb is connected'))
+  .catch(err => console.log(err));
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000!!');
+  console.log('Server running on port 3000');
 });
