6d50a87cf1c9be406a113832c9d13a103af5878a
SrTrace <levasho04sergey@gmail.com>
2024-11-16 21:28:56 +0200

    add upload image to client create-post

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -5,7 +5,7 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
   if (!token) {
-    console.log('token ', token);
+    // console.log('token ', token);
     return next(errorHandler(401, "Unauthorized"));
   }
 
