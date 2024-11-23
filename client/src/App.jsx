8d7866f3ffd3b388fa2a7c3a47a4e3e98e4aecda
SrTrace <levasho04sergey@gmail.com>
2024-11-23 13:08:04 +0200

    add update post logic to client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Footer from "./components/Footer";
 import PrivetRoute from "./components/PrivetRoute";
 import OnlyAdminPrivetRoute from "./components/OnlyAdminPrivetRoute";
 import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
 
 export default function App() {
   return (
@@ -25,6 +26,7 @@ export default function App() {
         </Route>
         <Route element={<OnlyAdminPrivetRoute />}>
           <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
