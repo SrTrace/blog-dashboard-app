105dcd06541274f7d5570e6d2be6e893048c9af4
SrTrace <levasho04sergey@gmail.com>
2024-11-27 11:18:25 +0200

    add commet component and create route comment for api

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,5 +1,6 @@
 import { errorHandler } from "../utils/error.js";
 import Comment from "../models/comment.model.js";
+import e from "express";
 
 export const createComment = async (req, res, next) => {
   try {
@@ -18,6 +19,19 @@ export const createComment = async (req, res, next) => {
     await newComment.save();
 
     res.status(200).json(newComment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPostComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({
+      postId: req.params.postId
+    })
+      .sort({ createdAt: -1 });
+    
+    res.status(200).json(comments);
   } catch (error) {
     next(error);
   }
